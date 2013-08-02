8c0c06823b93b77ca4b3e179ec645ce09f209023
Sergey Storchay <r8@r8.com.ua>
2013-08-02 08:21:05 +0300

    Add default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,3 +7,9 @@
 # Licensed under MIT:
 # http://raw.github.com/r8/chef-phing/master/LICENSE.txt
 
+# Phing install method (pear)
+default["phing"]["install_method"] = "pear"
+
+# When installing via PEAR, this is the preferred state 
+# (stable, beta, devel) # or a specific x.y.z pear version (eg. 4.5.0).
+default["phing"]["version"] = "stable"
