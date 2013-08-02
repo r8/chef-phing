06362761a0173724f873827427a5a0478c0c9b41
Sergey Storchay <r8@r8.com.ua>
2013-08-02 08:17:27 +0300

    Add initial files

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: phing
+# Attributes:: default
+#
+# Copyright 2013, Sergey Storchay
+#
+# Licensed under MIT:
+# http://raw.github.com/r8/chef-phing/master/LICENSE.txt
+
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: phing
+# Recipe:: default
+#
+# Copyright 2013, Sergey Storchay
+#
+# Licensed under MIT:
+# http://raw.github.com/r8/php-phing/master/LICENSE.txt
+
+include_recipe "php"
+
