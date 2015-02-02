189f8bd52d69b943cff9c1a5f4df7d4811204888
Dieter Blomme <dieterblomme@gmail.com>
2015-02-02 16:40:58 +0100

    Complete refactor
    Rewrite pear recipe to make it simpler and use php_pear functionality better.
    User berkshelf for dependency management and easy testing with vagrant.
    Removed unnecessary files

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,7 +12,4 @@ default["phing"]["install_method"] = "pear"
 
 # When installing via PEAR, this is the preferred state 
 # (stable, beta, devel) or a specific x.y.z PEAR version (eg. 4.5.0)
-default["phing"]["version"] = "stable"
-
-# URL of allreleases.xml for pear to install from preferred states
-default["phing"]["allreleases"] = "http://pear.phing.info/rest/r/phing/allreleases.xml"
+default["phing"]["preferred_state"] = "stable"
