9ecef2a18777e21391734e4f76bec29f7bcd54d6
Dieter Blomme <dieterblomme@gmail.com>
2016-10-04 10:56:40 +0200

    Refactor composer install to user install_global resource

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,11 +10,11 @@
 # Phing install method (pear)
 default["phing"]["install_method"] = "composer"
 
-# When installing via PEAR, this is the preferred state 
+# When installing via PEAR, this is the preferred state
 # (stable, beta, devel) or a specific x.y.z PEAR version (eg. 4.5.0)
 default["phing"]["preferred_state"] = "stable"
 
 # Composer specific settings
-default['phing']['install_dir'] = '/usr/local/phing'
-default['phing']['prefix'] = '/usr/bin'
+default['phing']['install_dir'] = default['composer']['global_install']['install_dir']
+default['phing']['prefix'] = node['composer']['global_install']['bin_dir']
 default['phing']['version'] = 'latest'
